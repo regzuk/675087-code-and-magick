a258caebad1e11d08e369fa76a437d25a8f35ca0
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-06-06 17:33:14 +0300

    generate wizard

--- js/setup.js
+++ js/setup.js
@@ -5,4 +5,16 @@
   var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+  var getRandomArrayElement = function (arr) {
+    return arr[Math.floor(arr.length * Math.random())]
+  };
+  var generateWizard = function ()  {
+    return {
+      name: getRandomArrayElement(WIZARD_NAMES) + ' ' + getRandomArrayElement(WIZARD_SURNAMES),
+      coatColor: getRandomArrayElement(COAT_COLORS),
+      eyesColor: getRandomArrayElement(EYES_COLORS)
+    };
+  };
+
 })();
